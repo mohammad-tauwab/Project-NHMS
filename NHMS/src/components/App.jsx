2460b956ad6fb0ab7806106e3bf645fd518c439b
Mohammad-Tauwab <sahil4493@gmail.com>
2024-11-30 17:28:31 +0530

    change config file for tailwind

--- NHMS/src/components/App.jsx
+++ NHMS/src/components/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <h1 className={`${style.maintitle}`}>THis is demo page</h1>
       <h2 className={`${style.title}`}>Main Title</h2>
       <p className={`${style.para}`}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis reiciendis voluptatem neque dolorum vitae porro excepturi nisi cumque magnam harum. Error totam rerum omnis sequi. Ipsum cumque voluptas est neque!</p>
-      <p className='text-2xl text-red-500'> Tailwind style </p>
+      <p className='text-2xl text-red-400'> Tailwind style </p>
       </center>
     </>
   )
--- NHMS/tailwind.config.js
+++ NHMS/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/components/.*jsx'],
+  content: ["./src/components/*.jsx"],
   theme: {
     extend: {},
   },
